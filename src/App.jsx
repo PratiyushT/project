15f1cf8b80c02dd4883dd0f2ddde5f41daff6564
Pratiyush Tiwari <aloomataloo@gmail.com>
2023-02-18 22:13:25 +0545

    Fix css where divisions would flow out of body

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Main from "./components/Main";
 
 export default function App() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen space-x-5 bg-blue-400">
+    <div className="flex justify-center items-center w-screen h-full min-h-screen space-x-5 bg-blue-400 overflow-auto">
       <div className="bg-white rounded-lg p-10">
         <Main />
       </div>
